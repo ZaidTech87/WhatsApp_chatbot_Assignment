80346a093be741b0436c79dc2c547c7e79735b52
Mohd Zaid <zaidalig1234@gmail.com>
2026-04-25 12:28:07 +0530

    this is first by zaid

---
+++ WhatsApp_chatbot/src/test/java/com/assignment/WhatsApp_chatbot/WhatsAppChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.WhatsApp_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
