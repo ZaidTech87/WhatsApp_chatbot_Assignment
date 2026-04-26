43fecff2cf833ef8bab560a79cca880c229b5b97
Mohd Zaid <zaidalig1234@gmail.com>
2026-04-26 13:42:14 +0530

    hello22

--- WhatsApp_chatbot/src/main/java/com/assignment/WhatsApp_chatbot/service/ChatService.java
+++ WhatsApp_chatbot/src/main/java/com/assignment/WhatsApp_chatbot/service/ChatService.java
@@ -1,4 +1,23 @@
 package com.assignment.WhatsApp_chatbot.service;
 
+
 public class ChatService {
+
+    public static String getReply(String message) {
+
+        if (message == null) {
+            return "Invalid message";
+        }
+
+        message = message.trim().toLowerCase();
+
+        switch (message) {
+            case "hi":
+                return "Hello";
+            case "bye":
+                return "Goodbye";
+            default:
+                return "I don't understand";
+        }
+    }
 }
